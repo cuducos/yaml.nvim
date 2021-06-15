4d5376696116e1e53269a156548eb778d7fee78a
Eduardo Cuducos <cuducos@users.noreply.github.com>
2021-06-15 09:20:11 -0400

    Enhances init strategies

--- lua/yaml_nvim/init.lua
+++ lua/yaml_nvim/init.lua
@@ -1,11 +1,17 @@
+local document = require("yaml_nvim.document")
 local pair = require("yaml_nvim.pair")
 local M = {}
 
 M.view = function()
-  print(pair.parse().as_string)
+  print(pair.parse(document.get_key_relevant_to_cursor()).as_string)
 end
 
 M.init = function()
+  local has_telescope, telescope = pcall(require, "telescope")
+  if has_telescope then
+    telescope.load_extension("yaml")
+  end
+
   vim.cmd("command! YAMLView lua require('yaml_nvim').view()")
 end
 
