3cc094cc4e60ac425c1a70761aece618636b5b3e
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2022-07-01 19:04:04 -0400

    Adds test basics
    Thanks @vinibispo, @ellisonleao and Ctrl V Ctrl V — or, should I say, y p?

---
+++ tests/init.lua
@@ -0,0 +1,19 @@
+local install = function(repo, dir)
+    local is_not_a_directory = vim.fn.isdirectory(dir) == 0
+    if is_not_a_directory then
+        print(vim.fn.system({ "git", "clone", repo, dir }))
+    end
+    vim.opt.rtp:append(dir)
+end
+
+local plenary_dir = os.getenv("PLENARY_DIR") or "/tmp/plenary.nvim"
+local treesitter_dir = os.getenv("TREESITTER_DIR") or "/tmp/nvim-treesitter"
+
+install("https://github.com/nvim-lua/plenary.nvim", plenary_dir)
+install("https://github.com/nvim-treesitter/nvim-treesitter", treesitter_dir)
+
+vim.opt.rtp:append(".")
+vim.cmd("runtime plugin/plenary.vim")
+vim.cmd("runtime plugin/nvim-treesitter")
+vim.cmd("runtime plugin/yaml.vim")
+require("plenary.busted")
---
+++ tests/yaml_nvim/setup_spec.lua
@@ -0,0 +1,36 @@
+local load_cmds = function()
+    local cmds = {}
+    for _, cmd in pairs(vim.api.nvim_get_commands({})) do
+        table.insert(cmds, cmd.name)
+    end
+    return cmds
+end
+
+local contains = function(tbl, item)
+    for _, value in pairs(tbl) do
+        if value == item then
+            return true
+        end
+    end
+    return false
+end
+
+describe("setup", function()
+
+    it("makes commands available", function()
+        require("yaml_nvim")
+
+        local expected = {
+            "YAMLView",
+            "YAMLYank",
+            "YAMLYankKey",
+            "YAMLYankValue",
+            "YAMLQuickfix",
+        }
+
+        local cmds = load_cmds()
+        for _, cmd in pairs(expected) do
+            assert(contains(cmds, cmd), cmd .. " not found")
+        end
+    end)
+end)
