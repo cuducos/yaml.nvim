be021cc68ac16880a1207dfa76ede074b3f605e8
Eduardo Vicente Gonçalves <4732915+cuducos@users.noreply.github.com>
2023-11-01 16:35:21 -0400

    Adds tests to yanking values

--- tests/yaml_nvim/commands_spec.lua
+++ tests/yaml_nvim/commands_spec.lua
@@ -16,4 +16,24 @@ describe("commands", function()
 
 		assert(vim.fn.getreg("7"), "worldcup")
 	end)
+
+	it("yanks value to the default register", function()
+		require("yaml_nvim")
+
+		vim.cmd(":e sample.yaml")
+		vim.cmd("norm 10j")
+		vim.cmd(":YAMLYankValue")
+
+		assert(vim.fn.getreg('"'), "7")
+	end)
+
+	it("yanks value to a custom register", function()
+		require("yaml_nvim")
+
+		vim.cmd(":e sample.yaml")
+		vim.cmd("norm 10j")
+		vim.cmd(":YAMLYankValue 7")
+
+		assert(vim.fn.getreg("7"), "7")
+	end)
 end)
